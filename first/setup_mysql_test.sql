965c07e46a298db627623bc369b9319c6062c05a
Ngobidi <chibuzormaxwell@gmail.com>
2024-01-24 05:11:46 -0800

    0x02. AirBnB clone - MySQL

---
+++ first/setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- developed the server for the project
+-- draft a database hbnb_test_db and a new user hbnb_test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+
+GRANT
+SELECT
+	ON performance_schema.* TO 'hbnb_test' @'localhost';
+
+FLUSH PRIVILEGES;
